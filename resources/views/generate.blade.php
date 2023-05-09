1f5b5994e8829c100b19931b291b0ad6eadc1b85
Erik-Mfa <erikmfa190230@gmail.com>
2023-05-08 22:38:49 -0300

    generate QR code view change

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -14,26 +14,29 @@
 <div class="w-screen h-screen flex justify-center items-center flex-col">
   <!-- Formulário para a alteração dos dados sobre o John -->
   
-    <h1 class="mb-8 font-bold text-5xl sm:visible invisible">Virtual Card Generator</h1>
-    <form action="{{route('generate.update')}}" method="POST" class="mb-6 sm:visible invisible">
+    <h1 class="mb-10 font-bold text-5xl">Virtual Card Generator</h1>
+    <form action="{{route('generate.update')}}" method="POST" class="mb-10">
       @csrf
       <div class="mb-6 w-96">
-        <label for="nome" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-        <div class="relative mt-2 rounded-md shadow-sm mb-2">
-          <input type="text" value='{{ $john->nome }}' name="nome" id="nome" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
+        <div class="relative rounded-md shadow-sm mb-2">
+          <label for="nome" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+          <input type="text" value='{{ $john->nome }}' name="nome" id="nome" class="focus:scale-105 ease-out duration-300 block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6">
         </div>
 
-        <label for="linkedin" class="block text-sm font-medium leading-6 text-gray-900">Linkedin URL</label>
-        <div class="relative mt-2 rounded-md shadow-sm mb-2">
-          <input type="text" value="{{ $john->linkedin }}" name="linkedin" id="linkedin" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        
+        <div class="relative mt-4 rounded-md shadow-sm mb-2">
+          <label for="linkedin" class="block text-sm font-medium leading-6 text-gray-900">Linkedin URL</label>
+          <input type="text" value="{{ $john->linkedin }}" name="linkedin" id="linkedin" class="focus:scale-105 ease-out duration-300 block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6">
         </div>
 
-        <label for="linkedin" class="block text-sm font-medium leading-6 text-gray-900">GitHub URL</label>
-        <div class="relative mt-2 rounded-md shadow-sm mb-8">
-          <input type="text" value="{{ $john->github }}" name="github" id="github" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        
+        <div class="relative mt-4 rounded-md shadow-sm mb-10">
+          <label for="linkedin" class="block text-sm font-medium leading-6 text-gray-900">GitHub URL</label>
+          <input type="text" value="{{ $john->github }}" name="github" id="github" class="focus:scale-105 ease-out duration-300 block w-full rounded-md border-0 py-1.5 pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6">
         </div>
 
-        <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button type="submit" class="hover:scale-125 ease-out duration-300 flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           Generate
         </button>
       </div>
